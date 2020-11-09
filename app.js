71c8e4a07405a677813763e184c8e7347dc0154b
Your-Full-Name <anh.dinhhoangquang@gmail.com>
2020-11-09 22:04:03 +0700

    sdas

--- app.js
+++ app.js
@@ -40,14 +40,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept'
+//     );
+//     next();
+// });
 const passportConfig = require('./config/passport');
 
 // init passport
