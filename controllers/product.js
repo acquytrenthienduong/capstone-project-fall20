947cfd55a00f5f988e1d1458c52b8b5a8ab5547e
ntt19981812 <tientainguyen.98@gmail.com>
2020-11-13 01:28:23 +0700

    post and get image

--- controllers/product.js
+++ controllers/product.js
@@ -3,38 +3,26 @@ const Product = db.product;
 // const op = db.Sequelize.Op;
 // const passport = require("passport");
 
-
-
-
-
-
-// create and save a new product
-exports.create = (req, res) => {
-  console.log("sssssss", req.body);
-  // Validate request
-  if (!req.body.productName) {
-    res.status(400).send({
-      message: "account name can not be empty!",
-    });
-    return;
-  }
-
-  // Create a product
-  const product = {
+// create a product
+exports.addProduct = (req, res) => {
+  console.log(req.file);
+  const product = new Product({
     productName: req.body.productName,
-    img_url: req.body.img_url,
+    img_url: req.file.path,
     sub_service_sub_service_id: req.body.sub_service_sub_service_id,
-  };
-
-  // Save product in the database
-  Product.create(product)
-    .then((data) => {
-      res.send(data);
+  });
+  product
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "Created product successfully",
+      });
     })
     .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the Product.",
+      console.log(err);
+      res.status(500).json({
+        error: err,
       });
     });
 };
@@ -42,16 +30,16 @@ exports.create = (req, res) => {
 // Retrieve all Products from the database.
 exports.findAll = (req, res) => {
   Product.findAll()
-    .then((data) => {
-      // console.log("data", data)
+  .then((data) => {
+      console.log("data", data)
       res.send(data);
-    })
-    .catch((err) => {
+  })
+  .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving Products.",
+          message:
+              err.message || "Some error occurred while retrieving products.",
       });
-    });
+  });
 };
 
 // Find a single Product with an id
@@ -95,27 +83,23 @@ exports.update = (req, res) => {
     });
 };
 
-
 // Delete a Product with the specified id in the request
 exports.delete = (req, res) => {
-    const id = req.params.id;
-    Manager.destroy({
-        where: { product_id: id },
-    })
-        .then((data) => {
-            console.log("data", data);
-            if (data == 1) {
-                res.status(200).send({
-                    message: "delete success",
-                });
-            }
-        })
-        .catch((err) => {
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while deleting product.",
-            });
+  const id = req.params.id;
+  Manager.destroy({
+    where: { product_id: id },
+  })
+    .then((data) => {
+      console.log("data", data);
+      if (data == 1) {
+        res.status(200).send({
+          message: "delete success",
         });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while deleting product.",
+      });
+    });
 };
-
-
